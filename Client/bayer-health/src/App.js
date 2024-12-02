82e982ff092d210733ca74fbf77b70d446e17005
AthiraGopinath1195 <athiragopinath1195@gmail.com>
2024-12-02 15:44:45 +0530

    Login UI

--- Client/bayer-health/src/App.js
+++ Client/bayer-health/src/App.js
@@ -1,12 +1,19 @@
 import "./App.css";
 import Header from "./components/Header";
 import Body from "./components/Body";
+import Login from "./components/Login";
+import { useState } from "react";
 
 function App() {
-  return (
+  const [isValidUser, setValidUser] = useState(false);
+  return isValidUser ? (
     <div className="App">
       <Header />
       <Body />
+    </div>
+  ) : (
+    <div className="App">
+      <Login />
     </div>
   );
 }
---
+++ Client/bayer-health/src/components/Login.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const login = () => {
+  return (
+    <div class="login-container">
+      <div class="login-card">
+        <div class="logo">
+          <img
+            src="https://www.bayer.com/themes/custom/bayer_cpa/logo.svg"
+            width="80"
+            alt="Bayer Healthcare Logo"
+          />
+        </div>
+        <h1>Login</h1>
+        <form>
+          <input type="email" placeholder="Email" required="" />
+          <input type="password" placeholder="Password" required="" />
+          <button class="login-btn" type="submit">
+            Login
+          </button>
+        </form>
+        <div class="links">
+          <a href="#">Forgot Password?</a>
+          <br />
+          <br />
+          <a href="#">New User? Register here</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default login;
